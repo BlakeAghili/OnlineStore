279624001d8ee21cee2db21f621e1cad619fc4a3
Blake Aghili <aghili.babak@gmail.com>
2020-06-23 10:22:51 -0500

    Load All Products: From Angular side.

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -2,15 +2,25 @@ import { Product } from "./product.model";
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 
+const productUrl = '/api/products'
 @Injectable()
 export class Repository {
   product: Product;
+  products: Product[];
   constructor(private http: HttpClient) {
     // this.product = JSON.parse(document.querySelector("#data").textContent);
-    this.getProduct(1);
+    //this.getProduct(1);
+    this.getProducts(true);
   }
 
   getProduct(id: number) {
-    this.http.get<Product>("/api/products/" + id).subscribe(p => this.product = p);
+    this.http.get<Product>("/api/products/" + id)
+      .subscribe(p => this.product = p);
   }
+
+  getProducts(related = false) {
+    this.http.get<Product[]>(`${productUrl}?related=${related}`)
+      .subscribe(items => this.products = items);
+  }
+
 }
