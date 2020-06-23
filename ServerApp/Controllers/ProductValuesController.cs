b168de4c82eac05cd880c1016cc4793945a34314
Blake Aghili <aghili.babak@gmail.com>
2020-06-23 09:01:11 -0500

    Displaying Releated Data

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ServerApp.Models;
 
 namespace ServerApp.Controllers
@@ -22,9 +23,37 @@ namespace ServerApp.Controllers
         public Product GetProduct(long id)
         {
             // just to simulate a load delay over network... 
-            System.Threading.Thread.Sleep(2500);
+            //System.Threading.Thread.Sleep(2500);
 
-            return context.Products.Find(id);
+            Product result = context.Products
+                .Include(p => p.Supplier).ThenInclude(s => s.Products)
+                .Include(p => p.Ratings)
+                .FirstOrDefault(p => p.ProductId == id);
+
+            if (result != null)
+            {
+                if (result.Supplier != null)
+                {
+                    result.Supplier.Products = result.Supplier.Products.Select(p => new Product
+                    {
+                        ProductId = p.ProductId,
+                        Name = p.Name,
+                        Category =  p.Category,
+                        Description = p.Description,
+                        Price = p.Price
+                    });
+                }
+
+                if (result.Ratings != null)
+                {
+                    foreach (Rating r in result.Ratings)
+                    {
+                        r.Product = null;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
