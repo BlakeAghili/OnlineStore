0d96a5f9e973dfb46ae5254343963292e8ca3566
Blake Aghili <peopleofksmo@gmail.com>
2020-06-23 21:32:49 -0500

    HTTP POST on Angular side to call into REST Service from .NET CORE side.

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Repository} from "./models/repository";
 import {Product} from "./models/product.model";
+import {Supplier} from "./models/supplier.model";
 
 @Component({
   selector: 'app-root',
@@ -19,5 +20,17 @@ export class AppComponent {
 
   get products(): Product[] {
     return this.repo.products;
+  }
+
+  createProduct() {
+    this.repo.createProduct(new Product(0, "Scuba Mask", "Watersports", "Fish Stalking", 490.98, this.repo.products[0].supplier));
+  }
+
+  createProductAndSupplier() {
+    let s = new Supplier(0, "Rocket Shoe Corp", "Boston", "MA");
+    let p = new Product(0, "Rocket-Powered Shoes", "Running",
+      "Set a new record", 100, s);
+
+    this.repo.createProductAndSupplier(p, s);
   }
 }
